c237f2d8b42715796486cf43e16f390d7941ac9d
alumno <alumno>
2019-02-06 12:03:29 -0300

    ejercicios 8 y 9(sin terminar)

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,22 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	var numUno;
+	var numDos;
+	var dividir;
+
+	numUno=numeroDividendo.value;
+	numDos=numeroDivisor.value;
+
+
+
+	numUno= parseInt(numUno);	
+	numDos= parseInt(numDos);
 	
+  
+
+    dividir=numUno%numDos;
+
+	alert ("el resto es: " + dividir);
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,40 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+ var importe;
+ var porcentaje;
+ var result;
+ 
+ result=resultado.value;
+
+ importe=sueldo.value;
+ importe=parseInt(importe);
+ 
+ importe
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 	
 }
