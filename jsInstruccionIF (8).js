ee6a84fc2ae1c71b4ce19e5a6753898b39fb0994
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-12 12:23:33 -0300

    ej if 8 12/2

---
+++ jsInstruccionIF (8).js
@@ -0,0 +1,28 @@
+function mostrar()
+{
+//tomo la edad  
+
+var laEdad;
+var estado;
+
+laEdad=edad.value;
+estado=estadoCivil.value;
+
+if(laEdad<18)
+	if(estado="Casado" && estado="Divorciado")
+	{
+
+	}
+
+if(laEdad<17)
+	if(estado="Soltero")
+	{
+		alert("Es soltero y no es menor");
+	}
+
+
+
+	
+
+
+}//FIN DE LA FUNCIÓN
