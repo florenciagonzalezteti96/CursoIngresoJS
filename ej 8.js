8edecb2fb5c5b2c3c0a0b9b18991af334a19fdcc
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-13 12:24:09 -0300

    Add files via upload

---
+++ ej 8.js
@@ -0,0 +1,25 @@
+var laEdad;
+var estado;
+var mensaje;
+
+laEdad=edad.value;
+estado=estadoCivil.value;
+
+if(laEdad<17)
+{
+	if(estado=="Soltero")
+	{
+		mensaje="Es muy pequeño para NO ser soltero.";
+	}
+	else
+	{
+
+	}
+
+}
+
+	
+alert(mensaje);
+	
+
+EJ 8.
