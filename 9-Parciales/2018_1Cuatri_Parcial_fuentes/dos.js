0332d67018a2b70e00485b2e9621a82f2d3c5b66
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-13 09:24:39 -0300

    segundo parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
-  
+  var precioProd;
+  var recargo;
+  var precioFinal;
+
+  precioProd=prompt("Ingrese el precio del producto:");
+  precioProd=parseInt(precioProd);
+
+  recargo=precioProd*10/100;
+
+  precioFinal=precioProd+recargo;
+
+  alert("El precio del producto en efectivo es: $"+precioProd+", con tarjeta tiene un recargo del 10% que seria $"+recargo+" de recargo, costandole final $"+precioFinal);
+
 }
