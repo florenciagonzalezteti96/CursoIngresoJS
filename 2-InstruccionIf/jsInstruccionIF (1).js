729dac37c1da71934720a1dae5bc8bc934b6ed2e
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2019-02-17 20:36:55 -0300

    EJ IF 1 Clase de apoyo

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
 
+laEdad=edad.value;
 
+if(laEdad==15)
+{
+	alert("Niña bonita");
+}
+
+/*
+	operadores aritmeticos: +,-,/,*,%
+	"""		   relacionales: ==,!=,>,<,>=,<=
+	"""		   logicos: &&,!(no),||(o inclusiva)
+*/
 
 }//FIN DE LA FUNCIÓN
