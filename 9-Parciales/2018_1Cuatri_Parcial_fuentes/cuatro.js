56bd42a2063909e8c181244e7c02f14b61a966a7
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-17 22:56:57 -0300

    parcial numero cuatro

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,73 +1,35 @@
-/*Al realizar una compra, si compra de mas de dos productos se realiza un descuento del 10% y si supera los $2000
-se agrega un descuento adicional de 15%, si el pago es con tarjeta y no efectivo al precio final se le agrega un 10%
-de recargo.*/
+function mostrar() 
 
-function mostrar()
 {
-	var cantidad;
-	var precio;
-	var metodoPago;
-	var recargo;
-	var finalRecargo;
-	var descuentoDiez;
-	var primerDesc;
-	var descuentoQuince;
-	var segundoDesc;
-	var precioTotal;
-	var precioTotalDos;
-	//var mensaje;
+	var numUno;
+	var numDos;
+	var resta;
+	var suma;
 
-	cantidad=prompt("Ingrese cuantos productos busca comprar:");
-	alert("Si compra mas de dos productos se realizara un descuento del 10%.");
-	precio=prompt("Ingrese el precio total:");
-	alert("Si supera los $2000, se agregara un descuento adicional del 15%.");
-	metodoPago=prompt("Ingrese si su metodo de pago es: tarjeta o efectivo.");
-	alert("Si paga con tarjeta y no efectivo, al precio final se le agregara un 10% de recargo.");
+	numUno=prompt("Ingresar el primer numero:");
+	numDos=prompt("Ingresar el segundo numero:");
 
-	cantidad=parseInt(cantidad);
-	precio=parseInt(precio);
+	numUno=parseInt(numUno);
+	numDos=parseInt(numDos);
 
-	descuentoDiez=precio*10/100;
-	descuentoQuince=precio*15/100;
-	recargo=precio*10/100;
-
-	//mensaje="usted compro "+cantidad+" producto/s";
-
-	if(cantidad>2)
+	if(numUno==numDos)
 	{
-		primerDesc=precio-descuentoDiez;
-		precioTotal=primerDesc;
-		//mensaje="asi que tiene un 10% de descuento, ";
+		alert(""+numUno+""+numDos);
+	}
 
-		if(precio>2000)
+	if(numUno>numDos)
+	{
+		resta=numUno-numDos;
+		alert(resta);
+	}
+
+	else
+	{
+		suma=(numUno)+(numDos);
+
+		if(suma>10)
 		{
-			segundoDesc=primerDesc-descuentoQuince;
-			precioTotal=segundoDesc;
-			//mensaje="y ha superado los $2000, por lo que tiene un 15% de descuento.";
+			alert("La suma es "+suma+" y supero el 10.");
 		}
-    }
 
-	else
-	{
-		precioTotal=precio;
 	}
-			
-	if(metodoPago=tarjeta)
-	{
-		finalRecargo=precio+recargo;
-		precioTotal=finalRecargo;
-		//mensaje="Ha ingresado la tarjeta como metodo de pago, por lo que tiene un 10% de recargo.";
-	}
-
-	else
-	{
-		precioTotal=precio;
-	}
-
-	//mensaje="Su costo final es de"+precioTotal;
-
-	alert("El precio total es de: $"+precioTotal);
-
-
-
-}
