51967e38a6a0c024c8d76dcc9f4d86390140d62c
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-17 21:36:22 -0300

    E/S FINAL

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -1,8 +1,9 @@
 /*Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt) y Sumarlos.
-mostrar el resulto por medio de "ALERT"*/
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
 function sumar()
-{
+{	
 	var uno;
 	var dos;
 	var sumar;
@@ -15,5 +16,57 @@ function sumar()
 
 	sumar=uno+dos;
 
-	alert("La suma total es: "+ sumar);
+	alert(sumar);
+
+}
+
+function restar()
+{
+	var uno;
+	var dos;
+	var restar;
+
+	uno=numeroUno.value;
+	dos=numeroDos.value;
+
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+
+	restar=uno-dos;
+
+	alert(restar);
+}
+
+function multiplicar()
+{ 
+	var uno;
+	var dos;
+	var multiplicar;
+
+	uno=numeroUno.value;
+	dos=numeroDos.value;
+
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+
+	multiplicar=uno*dos;
+
+	alert(multiplicar);
+}
+
+function dividir()
+{
+	var uno;
+	var dos;
+	var dividir;
+
+	uno=numeroUno.value;
+	dos=numeroDos.value;
+
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+
+	dividir=uno/dos;
+
+	alert(dividir);
 }
