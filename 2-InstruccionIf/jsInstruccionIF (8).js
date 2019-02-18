c48a72b2cff1130dc9620faed30349a7e6cf3656
florenciagonzalezteti96 <florenciagt.uni@gmail.com>
2019-02-17 21:11:02 -0300

    Final ejercicio

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+	var laEdad;
+	var estado;
+	laEdad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
 
-	
-
+	if ((laEdad>=18) && (estado=="Soltero"))
+	{
+		alert("Es soltero y no es menor.");
+	}
 
 }//FIN DE LA FUNCIÓN
