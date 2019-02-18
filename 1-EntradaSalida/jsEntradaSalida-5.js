8b804402fbe028ff538e6517f27857ceb146064c
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-17 21:35:18 -0300

    E/S FINAL

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,21 +2,12 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	//var nombre;
-	//var edad;
-
-	//elNombre.value=nombre;
-	//laEdad.value=edad;
-
-	//alert("su nombre es " + nombre + " y su edad es " + edad);
-
 	var nombre;
 	var edad;
 
 	nombre=elNombre.value;
 	edad=laEdad.value;
 
-	alert ("su nombre es " + nombre + " y su edad es " + edad);
+	alert("Su nombre es " + nombre + " y su edad es " + edad);
 	
 }
-
