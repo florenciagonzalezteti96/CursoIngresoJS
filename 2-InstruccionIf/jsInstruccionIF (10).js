24868fba050543eea9d6c76234d4a061ca1d8ae3
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-17 21:28:48 -0300

    Final ejercicio

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+	var mensaje;
+	numeroRandom=Math.floor((Math.random() * 11) + 1);
+
+	if (numeroRandom>4)
+	{
+		mensaje="APROBÓ";
+
+		if (numeroRandom==9 || numeroRandom==10)
+		{
+			mensaje="EXCELENTE";
+		}
+	}
+
+	else
+	{
+		mensaje="Vamos, la proxima se puede";
+	}
+
+alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
