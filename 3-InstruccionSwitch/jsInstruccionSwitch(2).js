0b0fbd87c5f0b1e16a1f7a4611d83cf77d7fde8f
alumno <alumno>
2019-02-19 11:41:38 -0300

    Clase del 19/2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,10 +2,49 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+var mensaje;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Enero":
+	mensaje="Falta para el invierno."
+		break;
+	case "Febrero":
+	mensaje="Falta para el invierno."
+		break;
+	case "Marzo":
+	mensaje="Falta para el invierno."
+		break;
+	case "Abril":
+	mensaje="Falta para el invierno."
+		break;
+	case "Mayo":
+	mensaje="Falta para el invierno."
+		break;
+	case "Junio":
+	mensaje="Falta para el invierno."
+		break;
+	case "Julio":
+	mensaje="Abrigate que hace frio."
+		break;
+	case "Agosto":
+	mensaje="Abrigate que hace frio."
+		break;
+	case "Septiembre":
+	mensaje="Ya pasamos el frio, ahora calor!!!."
+		break;
+	case "Octubre":
+	mensaje="Ya pasamos el frio, ahora calor!!!."
+		break;
+	case "Noviembre":
+	mensaje="Ya pasamos el frio, ahora calor!!!."
+		break;
+	case "Diciembre":
+	mensaje="Ya pasamos el frio, ahora calor!!!."
+		break;
 
+}
 
-
+alert (mensaje);
 
 }//FIN DE LA FUNCIÓN
