d3a7794d4213a2c30afeb3e1cf297b1214aabc59
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-20 09:27:13 -0300

    20/2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+	var planeta;
+	var mensaje;
+
+	planeta=prompt("Ingrese un planeta del sistema solar (en minusculas):");
+
+	switch(planeta)
+	{
+		case "tierra":
+		mensaje="acá vivimos";
+			break;
+		case "mercurio":
+		case "venus":
+		mensaje="acá hace más calor";
+			break;
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "neptuno":
+		case "urano":
+		mensaje="acá hace más frio";
+			break;
+		default:
+		mensaje="no es un planeta valido";
+			break;
+	}
+
+alert(mensaje);
 
 }
