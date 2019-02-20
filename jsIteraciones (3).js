cc799dc3f539c5e242b7805c94ead1e79d060857
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-20 12:22:52 -0300

    Add files via upload

---
+++ jsIteraciones (3).js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+
+var clave = prompt("ingrese el número clave.");
+
+while(clave!="utn750")//lo que no sea la clave entra aca, con lo que el while pide de nuevo la clave.
+
+{
+	clave="utn750";
+	console.log(clave);//
+}
+
+}//FIN DE LA FUNCIÓN
+//REALIZAR TP13.
