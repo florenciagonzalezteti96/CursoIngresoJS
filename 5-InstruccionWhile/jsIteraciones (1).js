1d472b0dc26a5cb8ad6a151fc0c4d270528f7a7e
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 11:39:48 -0300

    23/02

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,10 +1,10 @@
-function mostrar()
+/*function mostrar()
 {
 	alert('iteración while');
 /*toda iteracion tiene un comienzo, 
 un recorrido y una accion que marca su final*
 inicializacion, comparacion y cambio de valores.
-*/
+
 //si falla el cambio de valores, puede tildarse la maquina
 //si fallan la comparacion o la inicializacion no se realiza el WHILE
 
@@ -20,7 +20,7 @@ while(contador<10)//numero de repeticiones)//
 //con cada vuelta el while esta usando el microprocesador.
 //el numero que queda afuera del while es el 10,
 //porque si no entro en el while, el numero NO es menor a 10.
-para que reste el while*/
+para que reste el while
 var contador;
 contador=10;
 
@@ -34,6 +34,20 @@ while(contador>0)//-->el numero de la condicion es el numero con el que
 /*
 se utiliza para validacion de datos. cuando vuelve a pedir el dato. 
 */
+
+
+}//FIN DE LA FUNCIÓN*/
+
+function mostrar()
+{
+	var contador;
+	contador=0
+
+	while(contador<10)
+	{
+		contador=contador+1;//operacion ANTES DEL ALERT.
+		alert(contador);
+	}
 
 
 }//FIN DE LA FUNCIÓN
