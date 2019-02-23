d0e17aad3ca26a68981182b950296f53500003ce
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 11:40:56 -0300

    23/02

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador=11
+
+	while(contador>1)
+	{
+		contador=contador-1;//operacion ANTES DEL ALERT.
+		alert(contador);
+	}
 
 
 }//FIN DE LA FUNCIÓN
