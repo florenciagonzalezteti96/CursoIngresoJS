00ddcd576c4a592bf7fb4ed751c012ee54e56c2b
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 11:25:52 -0300

    23/02

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,31 @@
 function mostrar()
 {
-//tomo la edad  
+ 
 var mesDelAño = document.getElementById('mes').value;
-
+var mensaje;
+//7-31
+//4-30 ab jun sep nov
+//1-28
 //alert (mesDelAño);
-	
+switch(mesDelAño)
+{
+	case "Febrero":
+	mensaje="Este mes tiene 28 dias.";
+		break;
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":
+	mensaje="Este mes tiene 30 dias.";
+		break;
+	default:
+	mensaje="Este mes tiene 31 dias.";
+		break;
+}
+
+alert(mensaje);
+
+
 	
 
 
