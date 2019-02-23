8c434f015d96278fb5420eedca3e5f469cf6915a
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 11:36:29 -0300

    23/02

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,67 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var uno;
+	var dos;
+	var tres;
+	var sumar;
+
+	uno=precioUno.value;
+	dos=precioDos.value;
+	tres=precioTres.value;
+
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+	tres=parseInt(tres);
+
+	sumar=uno+dos+tres;
+
+	alert("La suma total es de: "+ sumar);
+
 }
+
 function Promedio () 
 {
-	
+	var uno;
+	var dos;
+	var tres;
+	var prom;
+
+	uno=precioUno.value;
+	dos=precioDos.value;
+	tres=precioTres.value;
+
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+	tres=parseInt(tres);
+
+	prom=(uno+dos+tres)/3;
+
+	alert("El promedio total es de: "+ prom);
 }
+
 function PrecioFinal () 
 {
-	
+	var uno;
+	var dos;
+	var tres;
+	var sumar;
+	var iva;
+	var precioFinal;
+
+	uno=precioUno.value;
+	dos=precioDos.value;
+	tres=precioTres.value;
+
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+	tres=parseInt(tres);
+
+	sumar=uno+dos+tres;
+
+	iva=sumar*21/100;
+
+	precioFinal=sumar+iva;
+
+	alert("La suma total es de: "+ precioFinal);
 }
