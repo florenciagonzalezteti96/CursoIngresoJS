70e067f351c1385817f748e3e30019cc9fe8ad5a
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 13:05:03 -0300

    23/02

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,81 @@
-function mostrar()
+/*function mostrar()
 {
 
 	var contador=0;
 	// declarar variables
+	var numero;
+	var maximo;
+	var minimo;
+	var cantidad=0;
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		numero=prompt("Ingrese un numero");
+		if(cantidad==0)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero<minimo)
+			{
+				minimo=numero;
+			}
+		}
+		cantidad ++; //incrementa en 1.
+		respuesta=prompt("¿Desea ingresar otro numero?");	
 	}
 
+	document.getElementById('maximo').value=maximo;
+	documento.getElementById('minimo').value=minimo;
 
+
+
+
+}//FIN DE LA FUNCIÓN*/
+
+//CON BANDERA (FLAG)
+function mostrar()
+{
+
+	var contador=0;
+	var numero;
+	var maximo;
+	var minimo;
+	var flag=false;
+	var respuesta='si';
+
+	while(respuesta!='no')
+	{
+		numero=prompt("Ingrese un numero");
+		if(flag==false || numero>maximo)
+		{
+			maximo=numero;
+		}
+		if(flag==false || numero<minimo)
+		{
+			minimo=numero;
+			flag=true;
+		}
+		
+		cantidad ++; //incrementa en 1.
+		respuesta=prompt("¿Desea ingresar otro numero?");	
+	}
+
+	document.getElementById('maximo').value=maximo;
+	documento.getElementById('minimo').value=minimo;
+
+
+
+
+}//FIN DE LA FUNCIÓN
 
 
 }//FIN DE LA FUNCIÓN
