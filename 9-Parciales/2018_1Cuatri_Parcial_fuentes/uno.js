78fe6f8c672fef1afcea0d4cb4113e76bcb146ca
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 11:44:47 -0300

    23/02

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt("Ingrese el ancho: ");
+	largo=prompt("Ingrese el largo");
+
+	perimetro=(largo*ancho);
+
+	alert("El perimetro del rectángulo es: "+ perimetro);
+	
 
 }
