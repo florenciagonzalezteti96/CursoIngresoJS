8d543f15ef9b57563c39fadf717b68b713f2b47a
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-23 12:17:53 -0300

    23/02

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave;
+clave=prompt("Ingrese el número clave.");
+
+while(clave!="utn750")//se ingresa lo que no quiero que se cumpla 
+					//o lo que quiero que se cumpla y negarlo.)
+{
+	alert("Clave incorrecta, reingrese la clave.");
+	clave=prompt("Reingrese el número clave.");
+}
+
+alert("Bienvenido/a.");
+
+//validacion de datos.
 
 
 }//FIN DE LA FUNCIÓN
