3dafb2e04dff6fef7866972d1e6947bcdb7b67e2
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-26 10:54:25 -0300

    26/02
    Agregados apuntes de la clase

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -18,3 +18,21 @@ document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
+
+/*
+	El contador suma una constante a la 
+	variable. 
+	cont= cont+1; o cont+=1; o cont++;(contador de post incremento)
+										nos muestra el valor y despues le agrega uno.
+	contador de preincremento ++cont (suma primero y muestra despues)
+
+	acumulador--> variable mas una variable.
+	acum=acum+num
+	acum+=num
+	acum*=num;
+	
+	flag o bandera-->variable con valor determinado
+	flag=true;
+	flag=false;
+	para saber si se dio una variable o no.
+*/
