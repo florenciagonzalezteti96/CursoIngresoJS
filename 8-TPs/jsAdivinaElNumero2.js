49295a445377d67c110afe82722bffb8591234ab
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-26 12:21:42 -0300

    26/02
    Necesita revision

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,19 +10,71 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
+var numeroUsuario;
 var contadorIntentos;
+var mensaje;
+
+contadorIntentos=0;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
+	/*	Genero el número RANDOM entre 1 y 100.
+		comparo con el dato ingresado en id="numero"
+		ver si es igual(“Usted es un ganador!!! y en solo X intentos”.), menor (“falta…”), o mayor (“se pasó…”).
+	*/
+		numeroSecreto=Math.floor((Math.random() * 101) + 1);
+		numeroSecreto=parseInt(numeroSecreto);
+		
 
 }
 
-function verificar()
+function verificar()//esta funcion simula un while
 {
 	
+	numeroUsuario=document.getElementById('numero').value;
+	numeroUsuario=parseInt(numeroUsuario);
+
+	contadorIntentos++;
+	document.getElementById('intentos').value=contadorIntentos;
+
+	if(numeroUsuario==numeroSecreto)
+	{
+		switch(contadorIntentos)
+		{
+			case 1:
+			mensaje="usted es un Psíquico";
+				break;
+			case 2:
+			mensaje="excelente percepción";
+				break;
+			case 3:
+			mensaje="Esto es suerte";
+				break;
+			case 4:
+			mensaje="Excelente técnica";
+				break;
+			case 5:
+			mensaje="usted está en la media";
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			mensaje="falta técnica";
+				break;
+			default:
+			mensaje="afortunado en el amor!!";
+				break;
+		}
+		
+	}
+
 	
+
+
+
+alert(mensaje);
+
 
 }
