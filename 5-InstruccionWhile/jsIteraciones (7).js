9112b9027ff67dca456a892f68977d57a7b58ad3
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-26 00:49:50 -0300

    Necesita revision

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,6 +4,16 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+
+	while(respuesta=="si");
+	{
+		numero=prompt("Ingrese un numero:");
+		numero=parseInt(numero);
+		contador=contador+1;
+		acumulador=acumulador+numero;
+		respuesta=prompt("Desea ingresar otro numero? si/no");
+	}
 
 
 document.getElementById('suma').value=acumulador;
