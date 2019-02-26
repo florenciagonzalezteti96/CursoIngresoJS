1a52892cf113bcf9884263a15e9146b00dce57a1
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-26 10:56:03 -0300

    26/02

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,34 @@
 function mostrar()
 {
 
-	var contador=0;
+
+	var acumuladorPositivos=0;
+	var	acumuladorNegativos=1
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
 
+	while(respuesta=="si")
+	{
+		numero=prompt("Ingrese un numero:");
+		numero=parseInt(numero);
+		if(numero>0)
+		{
+			acumuladorPositivos=acumuladorPositivos+numero;	
+			document.getElementById('suma').value=acumuladorPositivos;
+		}
+		else
+		{
+			acumuladorNegativos=acumuladorNegativos*numero;
+			document.getElementById('producto').value=acumuladorNegativos;
+		}
+		
+		respuesta=prompt("Desea ingresar otro numero? si/no");
+	}
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+
+
+//document.getElementById('suma').value=acumuladorPositivos;
+//document.getElementById('producto').value=acumuladorNegativos;
 
 }//FIN DE LA FUNCIÓN
