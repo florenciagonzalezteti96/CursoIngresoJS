8edb2650da6c414c830d3808393340202858b97b
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-02-26 00:51:19 -0300

    Finalizado el ejercicio

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero;
+	numero=prompt("ingrese un número entre 0 y 10.");
 
+	while(numero<0 || numero>10)
+	{	
+		alert("Numero invalido");
+		numero=prompt("Reingrese un numero!");
+		console.log(numero);
+	}
+
+	document.getElementById('Numero').value=numero;
 
 }//FIN DE LA FUNCIÓN
