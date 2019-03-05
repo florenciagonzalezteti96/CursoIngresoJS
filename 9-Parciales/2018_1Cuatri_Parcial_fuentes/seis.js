be622f59da5f4e6afb3cd847b05bb113289daa0d
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2019-03-05 17:04:53 -0300

    revision

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,53 @@
 function mostrar()
 {
+	var laHora;
+	var mensaje;
+
+	laHora=document.getElementById('laHora').value;
+	laHora=parseInt(laHora);
+
+
+	switch (laHora)
+	{
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		mensaje="es de mañana";
+			break;
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		mensaje="es de tarde";
+			break;
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		mensaje="es de noche";
+			if(laHora<=24 && laHora>20)
+			{
+				mensaje=mensaje+". A dormir.";
+			}
+		break;
+
+			default:
+			mensaje="La hora no es valida.";
+			break;
+	}
+	alert(mensaje);
 
 }
